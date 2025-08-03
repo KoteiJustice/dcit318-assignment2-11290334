84703182c30744a04b5dadcf6d91fdd7c05ed1d1
Yung-Phantom <yungphantom019@gmail.com>
2025-08-03 22:39:38 +0000

    Added the shape area calculator

---
+++ ShapeArea/Circle.cs
@@ -0,0 +1,11 @@
+public class Circle : Shape{
+    public double Radius {get; set;}
+
+    public Circle(double radius){
+        Radius = radius;
+    }
+
+    public override double GetArea(){
+        return Math.PI * Radius * Radius;
+    }
+}
---
+++ ShapeArea/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program{
+
+    static void Main(){
+
+        Shape circ = new Circle(5.0);
+        Console.WriteLine("Circle's area : " + circ.GetArea());
+
+        Shape rect = new Rectangle(5.0,5.0);
+        Console.WriteLine("Rectangle's area : " + rect.GetArea());
+
+    }
+}
